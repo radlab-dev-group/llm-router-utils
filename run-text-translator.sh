e11610c048ace8418fe002c648078203f439f2bb
Paweł Kędzia <pawel@radlab.dev>
2025-12-15 01:21:47 +0100

    ```text Switch to module execution in run-text-translator.sh, quote host URL, lower workers to 10 and raise batch size to 3 ```

--- run-text-translator.sh
+++ run-text-translator.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#
-python3 llm_router_utils/cli/translate_texts.py \
-  --llm-router-host=http://192.168.100.65:8080 \
+python3 -m llm_router_utils.cli.translate_texts \
+  --llm-router-host="http://192.168.100.65:8080" \
   --model="speakleash/Bielik-11B-v2.3-Instruct" \
   --dataset-type jsonl \
   --dataset-path="/mnt/data2/data/datasets/huggingface/marmikpandya/mental-health/data.jsonl" \
   --accept-field input \
   --accept-field output \
-  --num-workers=16 \
-  --batch-size=2 \
+  --num-workers=10 \
+  --batch-size=3
